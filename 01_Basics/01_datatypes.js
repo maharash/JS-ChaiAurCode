f0c8fbfacde2dd6736e26aafe0b4eec8bebb0814
Vishal Bhupesh Patil <95968960+maharash@users.noreply.github.com>
2024-07-20 07:42:52 +0000

    practice files

---
+++ 01_Basics/01_datatypes.js
@@ -0,0 +1,18 @@
+"use strict";   //treat all js as a newer version
+//alert(3+3)  //we are using nodejs not a browser
+
+//datatypes
+
+//number =>2 to power 53
+//bigint =>
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+//symbol =>unique
+
+//object
+
+
+console.log(typeof null);
+console.log(typeof undefined);
---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,25 @@
+const accountId=144553
+let accountEmail="vishal@gmail.com"
+var accountPassword="11122233"
+accountCity="Jaipur"
+let accountState;
+
+
+accountEmail="hc@hc.com"
+accountPassword="223313"
+accountCity="Bengaluru"
+
+
+//accountId=2  //const never be changed once it is assigned
+
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+
+prefer not to use var 
+beacause of issue in block scope and functional scope 
+*/
+
