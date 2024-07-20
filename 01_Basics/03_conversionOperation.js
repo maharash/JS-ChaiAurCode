91ead3e547a12c17b0401eb52290ae37229c53d3
Vishal Bhupesh Patil <95968960+maharash@users.noreply.github.com>
2024-07-20 08:22:38 +0000

    commit

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score="33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber =Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//in case of boolean==
+// 1=> true
+//0=>false
+//" "=>false
+//"Hitesh"=>true
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
