922881281652d8ca80f8b8e98938c1f5eb143e82
Vishal Bhupesh Patil <95968960+maharash@users.noreply.github.com>
2024-07-25 07:26:17 +0000

    My

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,55 @@
+// Primitive
+//7 types: String,Number,Boolean,null,undefined,Symbol,BigInt
+
+const score=100
+const scorevalue=100.3
+
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail;
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber=3455677899345790987654n
+
+
+//Refernce (Non-Primitive):
+
+//Array,Objects,Functions
+
+const heros=["shaktiman","naagraj","doga"];
+
+let myObj={
+    name:"hitesh",
+    age:22,
+
+}
+
+const myFunction=function(){
+    console.log("Hello World");
+}
+
+console.log(typeof scoreValue);
+
+/*
+
+Primitive Datatypes :-    Return type
+String                  string
+Number                  number
+Boolean                 Boolean
+Null                    Object(****)
+Undefined               undefined
+BigInt                  bigint
+
+
+
+Non-Primitive datatypes:-
+
+Array-object
+Function-function(Object)
+Object-object
+*/
+
